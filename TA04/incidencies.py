fa89b776083c8e3b877acdeb834f86506088db0f
BielITBGitHub <biel.delarosa.7e6@itb.cat>
2025-11-17 09:24:26 +0100

    ta02-Biel-DeLaRosa-ASIXc1B

--- TA04/incidencies.py
+++ TA04/incidencies.py
@@ -1,1 +1,20 @@
+import csv
+import xml.etree.ElementTree as ET
 
+arrel = ET.Element("incidencies")
+
+with open("incidencies.csv", "r", encoding="utf-8") as f:
+    lector = csv.DictReader(f)
+
+    for fila in lector:
+        incidencia = ET.SubElement(arrel, "incidencia")
+
+        for camp, valor in fila.items():
+            camp_xml = camp.strip().replace(" ", "_").replace("?", "")
+            node = ET.SubElement(incidencia, camp_xml)
+            node.text = valor
+
+arbre = ET.ElementTree(arrel)
+arbre.write("incidencies.xml", encoding="utf-8", xml_declaration=True)
+
+print("XML creat correctament!")
